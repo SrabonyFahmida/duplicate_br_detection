65e0024460b1e39ebd906f686507a448003930bb
Fahmida Hossain <35165269+SrabonyFahmida@users.noreply.github.com>
2023-12-02 19:29:59 -0500

    Update embeddings_sbert.py

--- embeddings_sbert.py
+++ embeddings_sbert.py
@@ -1,25 +1,35 @@
-#   !pip install sentence-transformers
-
-
 import json
 import numpy as np
-
-# Load the JSON file
-with open('/mnt/data/bug-reports.json', 'r') as file:
-    bug_reports = json.load(file)
-    
-# Extract the descriptions (or the relevant field) from the bug reports
-bug_report_texts = [report['description'] for report in bug_reports]
-
 from sentence_transformers import SentenceTransformer
 
 # Initialize the SentenceBERT model
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-# Generate embeddings for each bug report
+# Function to read multiple JSON objects from a file
+def read_json_objects(file_path):
+    bug_reports = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            try:
+                report = json.loads(line)
+                bug_reports.append(report)
+            except json.JSONDecodeError:
+                continue  # Skip malformed JSON
+    return bug_reports
+
+# Load bug reports
+bug_reports = read_json_objects('/home/fhossain/replication_package/0_data/0_bug report collection/corpus and queries/continuum/bug-reports.json')
+
+# Extract 'title' and 'description' from bug reports and concatenate them
+bug_report_texts = []
+for report in bug_reports:
+    title = report.get('title', '')  # Get title if exists, else empty string
+    description = report.get('description', '')  # Get description if exists, else empty string
+    combined_text = title + ' ' + description  # Concatenate title and description
+    bug_report_texts.append(combined_text)
+
+# Generate embeddings for each combined bug report text
 embeddings = model.encode(bug_report_texts)
 
-
-
 # Save embeddings to a file
-np.save('/mnt/data/bug_report_embeddings.npy', embeddings)
+np.save('/home/fhossain/replication_package/0_data/0_bug report collection/corpus and queries/continuum/bug_report_embeddings.npy', embeddings)
